ac84c85b00935c572095d93338126e6d258db977
17neverends <118381764+17neverends@users.noreply.github.com>
2023-10-13 22:12:49 +0300

    Delete graphapp directory

--- graphapp/scripts/grafik.m
+++
@@ -1,9 +0,0 @@
-clear;clc;
-
- 
-x1 = eval('0');
-x2 = eval('pi'); 
-n = 100;
-x = linspace(x1, x2, n);
-y = cos(x);
-plot(x, y);
--- graphapp/scripts/point.m
+++
@@ -1,4 +0,0 @@
-clear;clc;
-x = linspace(1, 7, 100);
-y = x;
-plot(x, y);
